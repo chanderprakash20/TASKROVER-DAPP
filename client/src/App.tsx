9f7726a039e782f6c4f28be8c490b84f194c4c2a
chanderprakash20 <chander132002@gmail.com>
2024-01-23 12:53:08 +0530

    I update code of petra wallet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,21 @@
 // src/App.tsx
-
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import React from 'react';
 import DecentralizedMarketplace from './components/DecentralizedMarketplace';
 import './App.css';
+import { Col } from 'antd'; // Assuming you are using Ant Design Col
 
 function App() {
   return (
     <div className="App">
+      {/* Your existing content */}
       <DecentralizedMarketplace />
+
+      {/* Add the WalletSelector component */}
+      <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+        <WalletSelector />
+      </Col>
     </div>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,20 @@
 // src/index.tsx
-
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// Define the array of wallets
+const wallets = [new PetraWallet()];
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // Wrap your App component with AptosWalletAdapterProvider
+  <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AptosWalletAdapterProvider>,
   document.getElementById('root')
 );
